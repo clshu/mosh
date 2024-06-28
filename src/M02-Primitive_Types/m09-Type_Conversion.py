f7f2c40e4fc28f5b6e56e502749efb8c8bdb68e0
clshu <clshu2020@gmail.com>
2024-06-28 16:07:07 -0700

    2- Primitive Types

---
+++ src/M02-Primitive_Types/m09-Type_Conversion.py
@@ -0,0 +1,32 @@
+x = input("Enter a number: ")
+print(type(x))
+y = int(x) + 1
+print(f"x: {x}, y:{y}")
+
+# trick on bool(x)
+# Falsy
+# False
+# None
+# 0
+# 0.0
+# 0j
+# ""
+# []
+# ()
+# {}
+# set()
+# range(0)
+
+
+# int(x)
+# float(x)
+# str(x)
+# bool(x)
+# list(x)
+# tuple(x)
+# dict(x)
+# set(x)
+# frozenset(x)
+# complex(x)
+# bytes(x)
+# bytearray(x)
