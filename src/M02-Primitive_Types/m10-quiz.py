783adae67f56a9a66bc565023bf02466d8e3652c
clshu <clshu2020@gmail.com>
2024-06-28 16:16:02 -0700

    Add m10-quiz.py to M02

---
+++ src/M02-Primitive_Types/m10-quiz.py
@@ -0,0 +1,31 @@
+# Quiz 1
+# Python types
+# Number, String, List, Tuple, Dictionary, Set, Boolean
+# Number: int, float, complex
+# String: str
+# List: list
+# Tuple: tuple
+# Dictionary: dict
+# Set: set
+# Boolean: bool
+
+# Quiz 2
+# String Slicing
+# string[start:stop:step]
+# string[::] - all characters
+# string[start:] - from start to end
+# string[:stop] - from beginning to stop
+# string[::-1] - reverse string
+# string[::2] - every 2nd character
+# string[-1] - last character
+# string[-2] - 2nd last character
+# string[-1:-4:-1] - last 3 characters in reverse order
+# string[-1:-4:-2] - last 3 characters in reverse order with
+fruit = "Apple"
+print(fruit[1:-1])  # ppl
+
+# Quiz 3
+10 % 3  # 1
+
+# Quiz 4
+print(bool("False"))  # True
